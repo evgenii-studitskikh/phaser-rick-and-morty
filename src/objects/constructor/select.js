f74eb1d3a6a0df926f7072673ee3113f8b34710f
Evgeny Studitskikh <estrueall@gmail.com>
2019-01-13 23:19:51 +0400

    Constructor for head of character created

--- src/objects/constructor/select.js
+++ src/objects/constructor/select.js
@@ -3,17 +3,42 @@
 export default class Select {
 
   constructor(game){
-    this.arrow_right = game.add.button(950, 250, 'arrow');
+    this.textData = ['head1', 'head2', 'head3', 'head4', 'head5', 'head6', 'head7', 'head8', 'head9'];
+    this.currentIndex = 0;
+
+    this.arrow_right = game.add.button(950, 250, 'arrow', () => this.handleRightClick());
     this.arrow_left = game.add.button(800, 300, 'arrow', () => this.handleLeftClick());
 
-    this.selectValue = game.add.text(815, 235, 'bbb', { fontFamily: 'Arial', fontSize: 64, fill: '#ffffff' });
+    this.selectValue = game.add.text(808, 245, this.textData[this.currentIndex], { 
+      fontFamily: 'Arial', fontSize: 48, fill: '#ffffff' 
+    });
 
     this.arrow_right.scale.setTo(0.2, 0.2);
     this.arrow_left.scale.setTo(0.2, 0.2);
     this.arrow_left.rotation = 3.2;
+
+    this.head1 = game.add.sprite(400, 230, 'heads');
+    this.head1.frame = 0;
+    this.head1.scale.setTo(0.4, 0.4);
+
+    this.soundClick = game.sound.add('click');
   }
 
   handleLeftClick() {
-    this.selectValue.text = 'aaaa';
+    if (this.currentIndex > 0) {
+      this.soundClick.play();
+      this.currentIndex--;
+      this.selectValue.text = this.textData[this.currentIndex];
+      this.head1.frame = this.currentIndex;
+    }
+  }
+
+  handleRightClick() {
+    if (this.currentIndex < this.textData.length - 1) {
+      this.soundClick.play();
+      this.currentIndex++;
+      this.selectValue.text = this.textData[this.currentIndex];
+      this.head1.frame = this.currentIndex;
+    }
   }
 }
--- src/states/Preload.js
+++ src/states/Preload.js
@@ -7,12 +7,17 @@ export default class Preload extends Phaser.State {
     this.game.load.image('planet', 'assets/planet.png');
     this.game.load.image('rick', 'assets/rick.png');
     this.game.load.image('button', 'assets/button.png');
-    this.game.load.image('club', 'assets/club.jpg');
+    this.game.load.image('club', 'assets/club.png');
     this.game.load.image('frame', 'assets/frame.png');
     this.game.load.image('arrow', 'assets/arrow.png');
+    this.game.load.spritesheet('heads', 'assets/heads.png', 350, 315);
 
     this.load.audio('soundtrack', [
       'music/techno.mp3'
+    ]);
+
+    this.load.audio('click', [
+      'sounds/click.mp3'
     ]);
   }
 
