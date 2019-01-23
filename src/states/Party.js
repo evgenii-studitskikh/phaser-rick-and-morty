ea0356eb330db59c85036966c861b319c0b7dc3d
SPavelV <pashius2010@gmail.com>
2019-01-23 18:57:57 +0400

    add music party

--- src/states/Party.js
+++ src/states/Party.js
@@ -101,39 +101,82 @@ export default class Party extends Phaser.State {
   }
 
   create() {
+    //генерим комменты
     this.commentsBar(this.dataComment);
 
     this.moving = 0;
     const bgImgWidth = 2649;
     const bgImgHeight = 1632;
     const widthCommentsList = 400;
-    const bgWidth = window.innerWidth * window.devicePixelRatio;
-    const bgHeight = window.innerHeight * window.devicePixelRatio;
-    console.log('--- window.innerWidth * window.devicePixelRatio', bgWidth);
-    console.log('---window.innerHeight * window.devicePixelRatio', bgHeight);
 
+    //устанавливаем размеры игрового мира:
     this.world.setBounds(0, 0, bgImgWidth, bgImgHeight);
 
+    //задаем размеры и позицию камеры:
     this.camera.setSize(window.innerWidth - widthCommentsList, window.innerHeight);
     this.camera.setPosition((bgImgWidth - window.innerWidth + widthCommentsList)/2 , (bgImgHeight - window.innerHeight)/2);
 
+    //дабавляем спрайт фона:
     this.clubBg = this.add.sprite(0, 0, 'party-club-bg');
 
+    //добавляем обработку событий мыши:
     this.cursors = this.input.keyboard.createCursorKeys();
 
-    this.music1 = this.add.audio('TheFluHatinRap');
-    this.music2 = this.add.audio('GetSchwifty');
-    this.music3 = this.add.audio('LetMeOut');
-    this.music4 = this.add.audio('TheRickDance');
-    this.musicArr = [ this.music1,this.music2,this.music3,this.music4 ];
-    this.indexMusic = 0;
+    // v.2
+    // this.music = this.add.audio('all-tracks');
+    // this.music.addMarker('1track', 0, 46.5);
+    // this.music.addMarker('2track', 46.5, 50);
+    // this.music.addMarker('3track', 97, 90);
+    // this.music.addMarker('4track', 187, 33);
+    // this.music.addMarker('5track', 221, 57);
+    // console.log('---', this.music.markers);
+    // this.currentMarker = '1track';
+    // this.sound.setDecodedCallback(this.music, this.startMusic, this);
 
-    // this.sound.setDecodedCallback( this.musicArr, this.start, this);
+    //подключаем фоновую музыку:
+    this.music1 = this.add.audio('1track');
+    // this.music1.addMarker('1', 0, 279);
+    this.music2 = this.add.audio('2track');
+    // this.music2.addMarker('2', 0, 298);
+    this.music3 = this.add.audio('3track');
+    // this.music3.addMarker('3', 0, 52);
+    this.music4 = this.add.audio('4track');
+    // this.music4.addMarker('4', 0, 236);
+    this.music5 = this.add.audio('5track');
+    this.music6 = this.add.audio('6track');
+    this.music7 = this.add.audio('7track');
+    this.music8 = this.add.audio('8track');
+    this.music9 = this.add.audio('9track');
+    this.music10 = this.add.audio('10track');
+    this.music11 = this.add.audio('11track');
+    this.music12 = this.add.audio('12track');
+    this.music13 = this.add.audio('13track');
+    this.musicArr = [ this.music1,this.music2,this.music3,this.music4,this.music5,this.music6,this.music7,this.music8,this.music9,this.music10,this.music11,this.music12,this.music13 ];
+    this.indexMusic = 5;
+    this.sound.setDecodedCallback( this.musicArr, this.startMusic, this);
+    // this.sound.autoplay = true;
 
     this.input.onDown.add(this.toggle, this);
     // this.input.onDown.add(this.changeVolume, this);
     // this.input.onDown.add(this.changeMusic, this);
   }
+
+
+  startMusic() {
+    this.musicArr[this.indexMusic].play();
+    this.musicArr[this.indexMusic].onStop = this.changeMusic;
+
+    // this.musicArr[this.indexMusic].onMarkerComplete =  this.changeMusic();
+    // this.music.loopFull();
+    // this.music.loopFull();
+  }
+
+  changeMusic() {
+    console.log('---onStop' );
+    // this.indexMusic = this.indexMusic === this.musicArr.length - 1 ? 0 : ++this.indexMusic;
+    // this.musicArr[this.indexMusic].play();
+  }
+
 
   toggle() {
     this.moving = (this.moving === 0) ? this.moving = 1 : this.moving = 0;
@@ -173,11 +216,6 @@ export default class Party extends Phaser.State {
 
   }
 
-  start() {
-    this.musicArr[this.indexMusic].play();
-    this.musicArr[this.indexMusic].loopFull();
-  }
-
   changeVolume(pointer) {
     if (pointer.y < 300)
     {
@@ -189,11 +227,6 @@ export default class Party extends Phaser.State {
     }
   }
 
-  changeMusic() {
-      this.musicArr[this.indexMusic].stop();
-      this.indexMusic = this.indexMusic === this.musicArr.length - 1 ? 0 : ++this.indexMusic;
-      this.musicArr[this.indexMusic].play();
-      this.musicArr[this.indexMusic].loopFull();
-  }
+
 
 }
