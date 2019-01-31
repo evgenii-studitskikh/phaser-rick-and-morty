7b0d15df91dddac9a2c16556b928c4b74fab4426
PavelSolovev <pashius2010@gmail.com>
2019-02-01 02:30:38 +0400

    cap for mobile version

--- src/index.js
+++ src/index.js
@@ -8,23 +8,30 @@ import Sharing from './states/Sharing';
 
 class Game extends Phaser.Game {
 
-	constructor() {
-		super(
-			window.innerWidth,
-			window.innerHeight
-		);
+  constructor() {
+    super(
+      window.innerWidth,
+      window.innerHeight
+    );
 
-		this.state.add('Boot', Boot, false);
-		this.state.add('Preload', Preload, false);
-		this.state.add('Cutscene', Cutscene, false);
-		this.state.add('Enterence', Enterence, false);
-		this.state.add('Intro', Intro, false);
-		this.state.add('Party', Party, false);
-		this.state.add('Sharing', Sharing, false);
-
-		this.state.start('Boot');
-	}
-
+    this.state.add('Boot', Boot, false);
+    this.state.add('Preload', Preload, false);
+    this.state.add('Cutscene', Cutscene, false);
+    this.state.add('Enterence', Enterence, false);
+    this.state.add('Intro', Intro, false);
+    this.state.add('Party', Party, false);
+    this.state.add('Sharing', Sharing, false);
+    if (window.innerWidth <= 768) {
+      console.log('close');
+    } else {
+      this.state.start('Boot');
+    }
+  }
 }
 
-new Game();
+if (window.innerWidth <= 768) {
+  console.log('close');
+} else {
+  new Game();
+}
+
